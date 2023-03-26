2bd0283bca6962d1a364ec322ec0793ab46c73ba
EwItsCaydon <caydonnahm@gmail.com>
2023-03-25 23:10:33 -0400

    Update master_ui.lua

--- master_ui.lua
+++ master_ui.lua
@@ -697,7 +697,7 @@ function Library:create(options)
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0, 5, 0.5, 0),
 		Size = UDim2.new(0, 15, 0, 15),
-		Image = "rbxassetid://12902068169",
+		Image = "rbxassetid://12902122227",
 		Theme = {ImageColor3 = "StrongText"}
 	})
 
